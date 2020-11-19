fa776302ebdcd6eac8552b69953c74de828e0561
H4ns Br1x <h4nsbr1x@protonmail.com>
2020-11-19 12:38:27 +0100

    Move injection logic into its own class

---
+++ H4xbr1x/InjectedDll.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H4xbr1x
+{
+    public class InjectedDll
+    {
+        Process process = default;
+        IntPtr handle = default;
+        IntPtr stringoffset = default;
+        IPeFile pefile = default;
+        bool injected = false;
+        string filename = default;
+
+        public InjectedDll(string filename, Process process)
+        {
+            this.filename = filename;
+            this.process = process;
+        }
+        public void Inject()
+        {
+            if (injected)
+                return;
+
+            Console.WriteLine("Process {0:s}", process.processEntry.szExeFile);
+            var dllname = "kernel32.dll";
+            var modules = ProcessQuery.GetModuleList(process);
+            ProcessQuery.MODULEENTRY32 me = ProcessQuery.GetModuleByName(process, dllname);
+            Console.WriteLine("kernel32 dll name: {0:s}", me.szModule);
+            pefile = PeParser.ParseFromProcess(process, me.modBaseAddr);
+            Console.WriteLine("Base address of kernel32.dll: {0:x}", (ulong)me.modBaseAddr);
+            Console.WriteLine("Dos signature: {0:x}", pefile.DosHeader.e_magic);
+            Console.WriteLine("Function names stored at {0:x}", pefile.ExportDirectory.AddressOfNames);
+            Console.WriteLine("Function addresses stored at {0:x}", pefile.ExportDirectory.AddressOfFunctions);
+            Console.WriteLine("Function ordinals stored at {0:x}", pefile.ExportDirectory.AddressOfNameOrdinals);
+            Console.WriteLine("Ordinal base: {0:d}", pefile.ExportDirectory.Base);
+            Console.WriteLine("LoadLibraryA offset: {0:x}", (ulong)pefile.Exports["LoadLibraryA"]);
+            Console.WriteLine("GetModuleHandleA offset: {0:x}", (ulong)pefile.Exports["GetModuleHandleA"]);
+            Console.WriteLine("GetModuleHandleW offset: {0:x}", (ulong)pefile.Exports["GetModuleHandleW"]);
+
+            //return;
+
+            // let's try injecting a dll
+
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("Can't load file {0:s}", filename);
+                return;
+            }
+
+            stringoffset = ProcessQuery.VirtualAllocEx(
+                process.hProcess,
+                (IntPtr)0,
+                (uint)filename.Length + 1,
+                ProcessQuery.AllocationType.Commit | ProcessQuery.AllocationType.Reserve,
+                ProcessQuery.MemoryProtection.ReadWrite);
+
+            ProcessQuery.WriteProcessMemory(process.hProcess, stringoffset, filename, filename.Length + 1, out _);
+            var hThread = ProcessQuery.CreateRemoteThread(process.hProcess, (IntPtr)0, 0, pefile.Exports["LoadLibraryA"], (IntPtr)stringoffset, 0, out _);
+
+            ProcessQuery.WaitForSingleObject(hThread, 0xFFFFFFFF);
+
+            ProcessQuery.GetExitCodeThread(hThread, out handle);
+
+            Console.WriteLine("Freeing memory at {0:x}", (ulong)stringoffset);
+            ProcessQuery.VirtualFreeEx(process.hProcess, stringoffset, 0, ProcessQuery.AllocationType.Release);
+
+            injected = true;
+            return;
+        }
+
+        public void UnInject()
+        {
+            if (!injected)
+                return;
+
+            var hThread = ProcessQuery.CreateRemoteThread(process.hProcess, (IntPtr)0, 0, (IntPtr)pefile.Exports["FreeLibrary"], handle, 0, out _);
+
+            ProcessQuery.WaitForSingleObject(hThread, 0xFFFFFFFF);
+
+            injected = false;
+        }
+    }
+}
--- H4xbr1xDemo/Program.cs
+++ H4xbr1xDemo/Program.cs
@@ -8,6 +8,7 @@ namespace H4xbr1xDemo
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             /*const string kernel32dump = "..\\..\\..\\..\\basicwindow32_kernel32.bin";
@@ -18,57 +19,19 @@ namespace H4xbr1xDemo
             }
             var kernel32mem = File.ReadAllBytes(kernel32dump);
             var peFile = PeParser.ParseFromMem(kernel32mem);*/
-            Process dummy = ProcessQuery.OpenProcessByName("BasicWindow64.exe");
-            Console.WriteLine("Process {0:s}", dummy.processEntry.szExeFile);
-            var dllname = "kernel32.dll";
-            var modules = ProcessQuery.GetModuleList(dummy);
-            ProcessQuery.MODULEENTRY32 me = ProcessQuery.GetModuleByName(dummy, dllname);
-            Console.WriteLine("kernel32 dll name: {0:s}", me.szModule);
-            var peFile = PeParser.ParseFromProcess(dummy, me.modBaseAddr);
-            Console.WriteLine("Base address of kernel32.dll: {0:x}", (ulong)me.modBaseAddr);
-            Console.WriteLine("Dos signature: {0:x}", peFile.DosHeader.e_magic);
-            Console.WriteLine("Function names stored at {0:x}", peFile.ExportDirectory.AddressOfNames);
-            Console.WriteLine("Function addresses stored at {0:x}", peFile.ExportDirectory.AddressOfFunctions);
-            Console.WriteLine("Function ordinals stored at {0:x}", peFile.ExportDirectory.AddressOfNameOrdinals);
-            Console.WriteLine("Ordinal base: {0:d}", peFile.ExportDirectory.Base);
-            Console.WriteLine("LoadLibraryA offset: {0:x}", (ulong)peFile.Exports["LoadLibraryA"]);
-            Console.WriteLine("GetModuleHandleA offset: {0:x}", (ulong)peFile.Exports["GetModuleHandleA"]);
-            Console.WriteLine("GetModuleHandleW offset: {0:x}", (ulong)peFile.Exports["GetModuleHandleW"]);
 
-            //return;
 
-            // let's try injecting a dll
-            const string dll = "C:\\Users\\User\\source\\repos\\H4xbr1x\\H4xbr1xDemo\\hl1dll64.dll";
+            Process dummy = ProcessQuery.OpenProcessByName("BasicWindow32.exe");
 
-            if (!File.Exists(dll))
-            {
-                Console.WriteLine("Can't load file {0:s}", dll);
-                return;
-            }
+            var dll = new InjectedDll("C:\\Users\\User\\source\\repos\\H4xbr1x\\H4xbr1xDemo\\hl1dll32.dll", dummy);
 
-            var stringOffset = ProcessQuery.VirtualAllocEx(
-                dummy.hProcess,
-                (IntPtr)0,
-                (uint)dll.Length + 1,
-                ProcessQuery.AllocationType.Commit | ProcessQuery.AllocationType.Reserve,
-                ProcessQuery.MemoryProtection.ReadWrite);
-
-            ProcessQuery.WriteProcessMemory(dummy.hProcess, stringOffset, dll, dll.Length+1, out _);
-            var hThread = ProcessQuery.CreateRemoteThread(dummy.hProcess, (IntPtr)0, 0, peFile.Exports["LoadLibraryA"], (IntPtr)stringOffset, 0, out _);
-
-            ProcessQuery.WaitForSingleObject(hThread, 0xFFFFFFFF);
-
-            IntPtr dllHandle;
-            ProcessQuery.GetExitCodeThread(hThread, out dllHandle);
-
-            Console.WriteLine("Freeing memory at {0:x}", (ulong)stringOffset);
-            ProcessQuery.VirtualFreeEx(dummy.hProcess, stringOffset, 0, ProcessQuery.AllocationType.Release);
+            dll.Inject();
 
             Console.ReadLine();
 
             Console.WriteLine("Unloading DLL");
 
-            ProcessQuery.CreateRemoteThread(dummy.hProcess, (IntPtr)0, 0, (IntPtr)peFile.Exports["FreeLibrary"], dllHandle, 0, out _);
+            dll.UnInject();
         }
     }
 }
